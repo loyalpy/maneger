51d37fff521ce2b2b405e9270499a5077ec22e39
py <1608626550@qq.com>
2018-08-27 18:35:32 +0800

    插件完善

--- plugins/adminLogin/adminLogin.php
+++ plugins/adminLogin/adminLogin.php
@@ -18,6 +18,16 @@ class adminLogin extends pluginBase
         return "管理员登录总部后台时，总部后台首页右下角弹出登录信息【平缘】";
     }
 
+    //配置相关信息
+    public static function configName()
+    {
+        return array(
+            "time" => array("name" => "自动隐藏信息", "type" => "select", "value" => array("开" => "on", "关" => "off")),
+            "loginInfo" => array("name" => "登录展示信息", "type" => "select", "value" => array("用户名" => "admin_name", "邮箱" => "email")),
+            "motto" => array("name" => "座右铭", "type" => "text", "value" => "一切美好只是昨日沉醉 淡淡苦涩才是今天滋味"),
+        );
+    }
+
     public function reg()
     {
         //视图监听是否存在新订单提醒
@@ -39,12 +49,19 @@ class adminLogin extends pluginBase
         //管理员登录
         $this->seller_id = ISafe::get('admin_name');
         $ajaxUrl = IUrl::creatUrl('/system/getAdminLoginInfo');
-
         $time = ITime::getDateTime();
+
+        //封装展示信息
+        $text = "登录账户：" . $this->seller_id . "<br/>";
+        $text .= "登录时间：" . $time . "<br />";
+        $text .= "登录设备：" . IClient::getDevice() . "<br />";
+        $text .= "登录地址：" . IClient::getIp() . "<br />";
+
         $randerData = array(
             "ajaxUrl" => $ajaxUrl,
             'time' => $time,
             'seller_id' => $this->seller_id,
+            'content' => $text
         );
         $this->view('adminLogin', $randerData);
     }
@@ -54,13 +71,6 @@ class adminLogin extends pluginBase
      */
     public function getAdminLoginInfo()
     {
-        $seller_id = IFilter::act(IReq::get('seller_id'), 'string');
-        $time = IFilter::act(IReq::get('time'));
-
-        $text = "登录账户：" . $seller_id . "<br/>";
-        $text .= "登录时间：" . $time . "<br />";
-        $text .= "登录设备：" . IClient::getDevice() . "<br />";
-        $text .= "登录地址：" . IClient::getIp() . "<br />";
-        die(JSON::encode(array('text' => $text)));
+        die(JSON::encode(array('text' => "用户登录信息")));
     }
 }
