462b8d6632925743dd85b08a5bb9d92afef59292
py <1608626550@qq.com>
2018-08-28 11:22:06 +0800

    钩子事件

--- plugins/adminLogin/adminLogin.php
+++ plugins/adminLogin/adminLogin.php
@@ -7,6 +7,7 @@
 class adminLogin extends pluginBase
 {
     private $seller_id = 0; //管理员
+    private $auto_hidden = "on"; //自动隐藏
 
     public static function name()
     {
@@ -22,8 +23,8 @@ class adminLogin extends pluginBase
     public static function configName()
     {
         return array(
-            "time" => array("name" => "自动隐藏信息", "type" => "select", "value" => array("开" => "on", "关" => "off")),
-            "loginInfo" => array("name" => "登录展示信息", "type" => "select", "value" => array("用户名" => "admin_name", "邮箱" => "email")),
+            "time" => array("name" => "自动关闭弹框", "type" => "select", "value" => array("开" => "on", "关" => "off")),
+            "loginInfo" => array("name" => "登录账号展示信息", "type" => "select", "value" => array("用户名" => "admin_name", "邮箱" => "email")),
             "motto" => array("name" => "座右铭", "type" => "text", "value" => "一切美好只是昨日沉醉 淡淡苦涩才是今天滋味"),
         );
     }
@@ -51,14 +52,10 @@ class adminLogin extends pluginBase
 
         //登录信息配置
         $login_info = (isset($configData['loginInfo']) && $configData['loginInfo']) ? $configData['loginInfo'] : 'admin_name';
-        if ($login_info == "admin_name"){//登录名
-            $this->seller_id = ISafe::get('admin_name');
-        }else{//登录邮箱
-            $this->seller_id = ISafe::get('admin_email');
-        }
+        $this->seller_id = ($login_info == "admin_name") ? ISafe::get('admin_name') : ISafe::get('admin_email');
 
         //隐藏开关
-        $order_cancel_time = (isset($configData['time']) && $configData['time']) ? $configData['time'] : 'on';
+        $this->auto_hidden = (isset($configData['time']) && $configData['time']) ? $configData['time'] : 'on';
 
         //登录时间
         $login_time = ISafe::get('admin_login_time');
@@ -74,7 +71,8 @@ class adminLogin extends pluginBase
                 "ajaxUrl" => IUrl::creatUrl('/system/getAdminLoginInfo'),
                 'time' => $login_time,
                 'seller_id' => $this->seller_id,
-                'content' => $text
+                'content' => $text,
+                'auto_hidden' => $this->auto_hidden
             );
             $this->view('adminLogin', $randerData);
         }
